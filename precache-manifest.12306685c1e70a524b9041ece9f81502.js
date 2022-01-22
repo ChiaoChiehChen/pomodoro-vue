58060e6e360edcb719f160efe31a6109d27b8506
ChiaoChiehChen <90738709+ChiaoChiehChen@users.noreply.github.com>
2022-01-22 12:55:02 +0000

    Deploying to gh-pages from @ ChiaoChiehChen/pomodoro-vue@c7915afee5fea8969f5c9430b4369dd814550198 🚀

---
+++ precache-manifest.12306685c1e70a524b9041ece9f81502.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb4caa95e1ed0f69d2b",
+    "url": "css/app.88617d4c.css"
+  },
+  {
+    "revision": "3369ef1dd1577ebdfa4a",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b654c04661ea6e6748097d4dbbcc05ca",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "4409c420ffe562e463c90baacadead95",
+    "url": "index.html"
+  },
+  {
+    "revision": "beb4caa95e1ed0f69d2b",
+    "url": "js/app.d1e268f7.js"
+  },
+  {
+    "revision": "3369ef1dd1577ebdfa4a",
+    "url": "js/chunk-vendors.443ffd69.js"
+  },
+  {
+    "revision": "fdc850657ca3026a468d",
+    "url": "js/list.09cf92dc.js"
+  },
+  {
+    "revision": "7b9f8767e77bfdc9ce08",
+    "url": "js/notfound.97bdcbb5.js"
+  },
+  {
+    "revision": "026d5ee92c90a05a6fd211b356d58d99",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "media/alarm.965b614d.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "media/yay.46c744f5.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
